ec9e174dda48b8f9d3e028bc8c5cb18706e6164b
NedduJS <nmamanipantoja@gmail.com>
2021-09-06 08:54:31 -0500

    Dynamic plant page Layout

---
+++ pages/entry/[slug].tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+import { Layout } from '@components/Layout'
+import { RichText } from '@components/RichText'
+import { AuthorCard } from '@components/AuthorCard'
+import { PlantEntryInline } from '@components/PlantCollection'
+import { Typography } from '@ui/Typography'
+import { Grid } from '@ui/Grid'
+
+const PlantEntryPage = () => {
+  const router = useRouter()
+  const slug = router.query.slug
+
+  return (
+    <Layout>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={8} lg={9} component="article">
+          <figure>
+            <Image
+              width={952}
+              aspectRatio="4:3"
+              layout="intrinsic"
+              src={plant.image.url}
+              alt={plant.image.title}
+            />
+          </figure>
+          <div className="px-12 pt-8">
+            <Typography variant="h2">{plant.plantName}</Typography>
+          </div>
+          <div className="p-10">
+            <RichText richText={plant.description} />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3} component="aside">
+          <section>
+            <Typography variant="h5" component="h3" className="mb-4">
+              {t('recentPosts')}
+            </Typography>
+            {otherEntries.map((plantEntry) => (
+              <article className="mb-4" key={plantEntry.id}>
+                <PlantEntryInline {...plantEntry} />
+              </article>
+            ))}
+          </section>
+          <section className="mt-10">
+            <Typography variant="h5" component="h3" className="mb-4">
+              {t('categories')}
+            </Typography>
+            <ul className="list">
+              {categories.map((category) => (
+                <li key={category.id}>
+                  <Link passHref href={`/category/${category.slug}`}>
+                    <Typography component="a" variant="h6">
+                      {category.title}
+                    </Typography>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </Grid>
+      </Grid>
+      <section className="my-4 border-t-2 border-b-2 border-gray-200 pt-12 pb-7">
+        <AuthorCard {...plant.author} />
+      </section>
+    </Layout>
+  )
+}
+
+export default PlantEntryPage
